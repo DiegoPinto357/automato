77a82186dfd222b87f1d5d703fbdd14e7a2c914d
DiegoPintoTW <diego.dossantos@thoughtworks.com>
2021-08-26 00:42:47 -0300

    Refactor update investments totals function.

--- src/investments/updateInvestmentsTotals.js
+++ src/investments/updateInvestmentsTotals.js
@@ -1,19 +1,41 @@
-const spreadsheetId = "1MuWkH84pJhQFxe07CHxPBGQTiSz6FDYCuPAB_DMbgNY";
-const sheetName = "Investimentos";
+const run = () => {
+  const spreadsheetId = '1MuWkH84pJhQFxe07CHxPBGQTiSz6FDYCuPAB_DMbgNY';
+  const sheetName = 'Investimentos';
 
-// rows
-const rowOffset = 2;
-const numOfRows = 14;
-const firstRow = rowOffset;
-const lastRow = firstRow + numOfRows - 1;
+  // rows
+  const rowOffset = 2;
+  const numOfRows = 14;
+  const firstRow = rowOffset;
+  const lastRow = firstRow + numOfRows - 1;
 
-// columns
-const columnOffset = 2;
-const numOfColumns = 13;
-const firstColumn = columnOffset;
-const lastColumn = firstColumn + numOfColumns;
+  // columns
+  const columnOffset = 2;
+  const numOfColumns = 13;
+  const firstColumn = columnOffset;
+  const lastColumn = firstColumn + numOfColumns;
 
-function updateInvestmentsTotals() {
+  const getTableData = () => {
+    const sheet =
+      SpreadsheetApp.openById(spreadsheetId).getSheetByName(sheetName);
+    const data = sheet.getDataRange().getValues();
+
+    const totalsRowIndex = lastRow + 1;
+    const newTotalsRowIndex = totalsRowIndex + 1;
+
+    const tableValues = data
+      .slice(firstRow, lastRow + 1)
+      .map(row => row.slice(firstColumn, lastColumn));
+
+    const currentTotals = data[totalsRowIndex].slice(firstColumn, lastColumn);
+    const newTotals = data[newTotalsRowIndex]
+      .slice(firstColumn, lastColumn)
+      .map((newTotal, index) =>
+        newTotal !== '' ? newTotal : currentTotals[index]
+      );
+
+    return { sheet, tableValues, currentTotals, newTotals };
+  };
+
   const { sheet, tableValues, currentTotals, newTotals } = getTableData();
   const totalDiffs = newTotals.map(
     (newTotal, index) => newTotal - currentTotals[index]
@@ -28,7 +50,7 @@ function updateInvestmentsTotals() {
 
     return values
       .map((value, index) => value + diff[index])
-      .map(value => (value == 0 ? "" : value));
+      .map(value => (value == 0 ? '' : value));
   });
 
   newValues.forEach((column, index) => {
@@ -39,26 +61,4 @@ function updateInvestmentsTotals() {
     const cells = sheet.getRange(range);
     cells.setValues(column.map(value => [value]));
   });
-}
-
-function getInvestmentsTableData() {
-  const sheet =
-    SpreadsheetApp.openById(spreadsheetId).getSheetByName(sheetName);
-  const data = sheet.getDataRange().getValues();
-
-  const totalsRowIndex = lastRow + 1;
-  const newTotalsRowIndex = totalsRowIndex + 1;
-
-  const tableValues = data
-    .slice(firstRow, lastRow + 1)
-    .map(row => row.slice(firstColumn, lastColumn));
-
-  const currentTotals = data[totalsRowIndex].slice(firstColumn, lastColumn);
-  const newTotals = data[newTotalsRowIndex]
-    .slice(firstColumn, lastColumn)
-    .map((newTotal, index) =>
-      newTotal !== "" ? newTotal : currentTotals[index]
-    );
-
-  return { sheet, tableValues, currentTotals, newTotals };
-}
+};
