3256585317b36e068c67e88960b772a74ed40cb3
Diego Pinto <diego.pinto357@gmail.com>
2021-09-24 19:06:09 -0300

    Get values from distribution and investments tables.

--- src/distribution/distributeToInvestments.js
+++ src/distribution/distributeToInvestments.js
@@ -1,16 +1,46 @@
-const distributeToInvestments = month => {
+const getDistributionValues = month => {
   const sheetName = 'Distribuição';
-  const distributionTableId = 'Distribuição';
+  const tableId = 'Distribuição';
 
-  const distributionTable = new Table(
-    spreadsheetId,
-    sheetName,
-    distributionTableId
-  );
+  const table = new Table(spreadsheetId, sheetName, tableId);
 
-  SpreadsheetApp.getUi().alert('distributeToInvestments ' + month);
+  const monthValues = table.getData().slice(2, -1);
 
-  console.log(distributionTable.getData());
+  return monthValues.reduce((dist, row, index) => {
+    if (index % 2 !== 0) {
+      dist.push(row.slice(1, -1)[month - 1]);
+    }
+    return dist;
+  }, []);
+};
+
+const getCurrentInvestmentValues = () => {
+  const sheetName = 'Investimentos';
+  const tableId = 'Finalidade';
+
+  const table = new Table(spreadsheetId, sheetName, tableId);
+
+  const ignoreAtTop = 1;
+  const ignoreAtBottom = 2;
+  const investmentColIndex = 1;
+
+  const data = table.getData();
+
+  return data.reduce((values, row, index) => {
+    if (index <= ignoreAtTop - 1 || index >= data.length - ignoreAtBottom)
+      return values;
+
+    values.push(row[investmentColIndex]);
+    return values;
+  }, []);
+};
+
+const distributeToInvestments = (month = 1) => {
+  const distributionValues = getDistributionValues(month);
+  const currentInvestmentValues = getCurrentInvestmentValues();
+
+  console.log(distributionValues);
+  console.log(currentInvestmentValues);
 };
 
 const showDistributionPrompt = () => {
@@ -25,7 +55,7 @@ const showDistributionPrompt = () => {
   const button = result.getSelectedButton();
   const month = result.getResponseText();
 
-  if (button == ui.Button.OK) {
+  if (button === ui.Button.OK) {
     if (month < 1 || month > 12) {
       ui.alert('Mês inválido');
       return;
